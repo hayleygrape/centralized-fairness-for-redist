4887df700a828e2b0b5e08c5826471df3fd3663c
hayleygrape <hg3@wellesley.edu>
2021-09-03 20:46:20 -0400

    m

--- chain.py
+++ chain.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from gerrychain import (GeographicPartition, Partition, Graph, MarkovChain, proposals, updaters, constraints, accept, Election,)
 from gerrychain.proposals import recom
+from gerrychain.updaters import Tally
 from gerrychain.updaters import cut_edges
 from functools import partial
 import pandas
@@ -19,22 +20,40 @@ os.makedirs(os.path.dirname(newdir + "init.txt"), exist_ok = True)
 with open(newdir + "init.txt", "w") as f:
     f.write("Created Folder")
 
-'''
+
 #networkx graph
 graph = Graph.from_json("./Data/PA_VTDALL.json")
-'''
+
 
 #make networkx graph
-graph = Graph.from_file("./Data/VTD_FINAL.shp")
+#graph = Graph.from_file("./Data/VTD_FINAL.shp")
 
 #make geopandas dataframe
-df = gpd.read_file("./Data/VTD_FINAL.shp")
+#df = gpd.read_file("./Data/VTD_FINAL.shp")
 
-num_districts = 19
+num_districts = 18
 
-initial_partition = Partition(graph, "2011_PLA_1")
+#election updater
+elections = [
+    Election("PRES16", {"D": "T16PRESD", "R": "T16PRESR"}),
+    Election("SEN16", {"D": "T16SEND", "R": "T16SENR"}),
+    Election("AG16", {"D": "T16ATGD", "R": "T16ATGR"}),
+    Election("GOV14", {"D": "F2014GOVD", "R": "F2014GOVR"})
+]
 
-print(initial_partition)
+electionNames = ["PRES16", "SEN16", "AG16", "GOV14"]
+
+parties = ["D", "R"]
+
+updaters = {
+    "population": updaters.Tally("TOT_POP", alias="population")
+}
+
+election_updaters = {election.name: election for election in elections}
+updaters.update(election_updaters)
+
+initial_partition = Partition(graph, "2011_PLA_1", updaters=updaters)
+
 
 totpop = 0
 for n in graph.nodes():
@@ -43,14 +62,15 @@ for n in graph.nodes():
 ideal_population = totpop / len(
     initial_partition
 )
-print(ideal_population)
 
+print(ideal_population)
 
 proposal = partial(
     recom, 
     pop_col="TOT_POP",
     pop_target=ideal_population,
-    epsilon=0.02,node_repeats=1
+    epsilon=0.02,
+    node_repeats=1
     )
 
 compactness_bound = constraints.UpperBound(
@@ -86,10 +106,46 @@ for partition in chain:
         #plt.close()
         #print("plot " + str(t) + " saved")
 
-        partition.graph.to_json(jsondir + "plot" + str(mapNum) + ".json")
+        d = {}
+
+        for i in range(num_districts):
+            d[i+1] = {}
+
+        for district in partition["population"]:
+            d[district]["population"] = partition["population"][district]
+            d[district]["id"] = []
+
+        for key in partition.assignment:
+            district = partition.assignment[key]
+            d[district]["id"].append(key)
+
         
+        for electionName in electionNames:
+
+            d[electionName] = {}
+            if partition[electionName].votes("D") > partition[electionName].votes("R"):
+                d[electionName]["winner"] = "D"
+            elif partition[electionName].votes("D") < partition[electionName].votes("R"):
+                d[electionName]["winner"] = "R"
+            else:
+                d[electionName]["winner"] = "TIE"
+            
+            d[electionName]["D"] = {}
+            d[electionName]["R"] = {}
+
+            for party in parties:
+                d[electionName][party]["seats"] = partition[electionName].seats(party)
+                d[electionName][party]["percent_wins"] = partition[electionName].percent(party)
+                d[electionName][party]["votes"] = int(sum(partition[electionName].counts(party)))
+
+
+        with open(jsondir + "plot" + str(mapNum) + ".json", 'w') as f:
+            json.dump(d, f)
+
         if mapNum % 10 == 0:
-            print("JSON for " + str(mapNum) + " saved")
+            currTime = time.time()
+            timeElapsed = (currTime - start) // 60
+            print("JSON for " + str(mapNum) + " saved in " + str(timeElapsed) + " minutes")
 
         mapNum += 1
 
