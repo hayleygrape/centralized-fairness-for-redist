f97d0b4643e40034cecfb663065c7e5970118b38
hayleygrape <hg3@wellesley.edu>
2021-10-21 15:50:18 +0100

    m

---
+++ election_hist.py
@@ -0,0 +1,57 @@
+import numpy as np
+import json
+import os
+import time
+import matplotlib.pyplot as plt
+
+numGraphs = 10
+
+sen16 = np.zeros(numGraphs)
+pres16 = np.zeros(numGraphs)
+ag16 = np.zeros(numGraphs)
+gov14 = np.zeros(numGraphs)
+
+start = time.time()
+
+for i in range(1, numGraphs+1):
+    fdir = "./JSON_Files2/plot" + str(i) + ".json"
+    with open(fdir) as a:
+        dict1 = json.load(a)
+
+    senSeats = int(dict1["SEN16"]["D"]["seats"])
+    presSeats = int(dict1["PRES16"]["D"]["seats"])
+    agSeats = int(dict1["AG16"]["D"]["seats"])
+    govSeats = int(dict1["GOV14"]["D"]["seats"])
+
+    sen16[i-1] = senSeats
+    pres16[i-1] = presSeats
+    ag16[i-1] = agSeats
+    gov14[i-1] = govSeats
+
+print(str(time.time()-start))
+
+print(sen16)
+print(pres16)
+print(ag16)
+print(gov14)
+
+
+
+numBins = [int(i) for i in range(15)]
+print(numBins)
+
+plt.hist(sen16, bins=numBins, density=True)
+plt.savefig("sen16_hist.png")
+plt.close()
+
+plt.hist(pres16, bins=numBins, density=True)
+plt.savefig("pres16_hist.png")
+plt.close()
+
+plt.hist(ag16, bins=numBins, density=True)
+plt.savefig("ag16_hist.png")
+plt.close()
+
+plt.hist(gov14, bins=numBins, density=True)
+plt.savefig("gov14_hist.png")
+plt.close()
