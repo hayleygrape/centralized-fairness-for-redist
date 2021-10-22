090ba0afd9a2460c5c45b33f2c3437072654c08c
hayleygrape <hg3@wellesley.edu>
2021-10-22 11:54:57 +0100

    takes command line argument

--- graphs.py
+++ graphs.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import geopandas as gpd
-
+import sys
 
 df = gpd.read_file("./Data/VTD_FINAL.shp")
 
@@ -11,26 +11,26 @@ df = gpd.read_file("./Data/VTD_FINAL.shp")
 numDistricts = 18
 mapNum = 10
 
-fdir = "./JSON_Files2/plot" + str(mapNum) + ".json"
+for mapNum in sys.argv[1:]:
+    fdir = "./JSON_Files2/plot" + str(mapNum) + ".json"
 
-with open(fdir) as a:
-    dict1 = json.load(a)
+    with open(fdir) as a:
+        dict1 = json.load(a)
 
 
-districts = [str(x+1) for x in range(numDistricts)]
-print(districts)
+    districts = [str(x+1) for x in range(numDistricts)]
 
-partition_assignment = {}
+    partition_assignment = {}
 
-for district in dict1:
-    if district in districts:
-        for vtd_id in dict1[district]["id"]:
-            partition_assignment[vtd_id] = int(district)
+    for district in dict1:
+        if district in districts:
+            for vtd_id in dict1[district]["id"]:
+                partition_assignment[vtd_id] = int(district)
 
-print(partition_assignment)
+    df["plot" + str(mapNum)] = df["GEOID10"].map(partition_assignment)
 
-df["plot" + str(mapNum)] = df["GEOID10"].map(partition_assignment)
+    df.plot(column='plot' + str(mapNum),cmap='tab20')
+    plt.savefig("plot" + str(mapNum) + "_image.png")
+    plt.close()
 
-df.plot(column='plot' + str(mapNum),cmap='tab20')
-plt.savefig("plot" + str(mapNum) + "_photo.png")
-plt.close()
+    print("Map " + str(mapNum) + " done")
